d6b601483f63c1eba21ad9759eba723fcdad006c
marcelo <hemakesnoise@gmail.com>
2013-11-23 15:44:20 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comparasalud::Application.config.secret_token = '332b184e755b730cbd8da43fe2422b701ead90eecc7405c4adb516e7d80668877719a8d603267dcd5fb5c1a5c101d2d5d95bff7f824c023946f7f0c72de144ac'
